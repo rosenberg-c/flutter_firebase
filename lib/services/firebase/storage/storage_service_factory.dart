dd76948e622630ee224c944766c8df988dbb2092
Christian <rosenberg-c>
2020-03-25 22:23:21 +0100

    remove cloud storage dependency

--- lib/services/firebase/storage/storage_service_factory.dart
+++ lib/services/firebase/storage/storage_service_factory.dart
@@ -1,17 +1,17 @@
-import 'storages/firebase_storage.dart';
-
-class StorageServiceFactory {
-  CloudStorage _instance;
-
-  CloudStorage _factory() {
-    if (_instance == null) {
-      _instance = CloudStorage();
-      return _instance;
-    }
-    return _instance;
-  }
-
-  CloudStorage get instance {
-    return _factory();
-  }
-}
+//import 'storages/firebase_storage.dart';
+//
+//class StorageServiceFactory {
+//  CloudStorage _instance;
+//
+//  CloudStorage _factory() {
+//    if (_instance == null) {
+//      _instance = CloudStorage();
+//      return _instance;
+//    }
+//    return _instance;
+//  }
+//
+//  CloudStorage get instance {
+//    return _factory();
+//  }
+//}
